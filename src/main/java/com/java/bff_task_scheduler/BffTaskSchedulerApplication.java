0fc9c97d60c9a79eb978dc848cf153de4a14bbac
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-03-18 15:50:31 -0300

    Initial commit

---
+++ src/main/java/com/java/bff_task_scheduler/BffTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.java.bff_task_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffTaskSchedulerApplication.class, args);
+	}
+
+}
